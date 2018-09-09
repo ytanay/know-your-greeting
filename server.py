44a158b8b7b2356cfd34a4b21b33b71d15672712
Yotam Tanay <yotam.tanay@forter.com>
2018-09-09 19:14:19 +0300

    Cleanup server

--- server.py
+++ server.py
@@ -4,10 +4,19 @@ import os
 from flask import Flask, render_template
 
 with open('lines.txt', 'r') as f:
-	lines = list(set(f.readlines()))
+	LINES = set(f.readlines())
 
-emoticons = '☺ ☻ ✌ ☹ ♡ ♥ ❤ ⚘ ❀ ❃ ❁ ✼ ☀ ✌ ♫ ♪ ☃ ❄ ❅ ❆'
-opening = ['לכבוד השנה החדשה', 'עם כניסת השנה החדשה', 'ובבוא השנה החדשה', 'יחד אל השנה החדשה', 'אספר לכם סיפור על השנה החדשה', 'נצעד אל פתח השנה החדשה', 'אין כמו שנה חדשה', 'רק היום נכנסת השנה']
+EMOTICONS = '☺ ☻ ✌ ☹ ♡ ♥ ❤ ⚘ ❀ ❃ ❁ ✼ ☀ ✌ ♫ ♪ ☃ ❄ ❅ ❆'
+OPENINGS = ['לכבוד השנה החדשה', 'עם כניסת השנה החדשה', 'ובבוא השנה החדשה', 'יחד אל השנה החדשה', 'אספר לכם סיפור על השנה החדשה', 'נצעד אל פתח השנה החדשה', 'אין כמו שנה חדשה', 'רק היום נכנסת השנה']
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html', greeting=make_greeting())
+
 
 def format_line(line):
 	sel = random.random()
@@ -16,29 +25,21 @@ def format_line(line):
 		return '<b>' + line + '</b>'
 
 	if sel < 0.5:
-		return line + ' ' + random.choice(emoticons)
+		return line + ' ' + random.choice(EMOTICONS)
 	return line
 
+
 def make_greeting(length=None):
-	all_lines = list(lines)
+	all_lines = list(LINES)
 
 	random.shuffle(all_lines)
 
 	if length is None:
 		length = random.randrange(4, 30) * 2
 
-	return random.choice(opening) + ',<br/>' + '<br>'.join(format_line(all_lines.pop().strip()) for x in range(length))
-
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-	return render_template('index.html', greeting=make_greeting())
+	return random.choice(OPENINGS) + ',<br/>' + '<br>'.join(format_line(all_lines.pop().strip()) for x in range(length))
 
 
 if __name__ == '__main__':
-	app.run(port=int(os.environ.get('PORT') or '8001'), host='0.0.0.0', debug=True)
+	app.run(port=int(os.environ.get('PORT') or '8000'), host='0.0.0.0', debug=True)
 
